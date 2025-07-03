ec2ac2f2c4f06bb84346922864af9eaf7436a9ea
nallari <shruti.nallari@gmail.com>
2025-07-03 01:07:39 -0400

    insert

--- New folder/insertionsort.js
+++ New folder/insertionsort.js
@@ -1,10 +1,26 @@
 class insertsort{
+    //take the first element
+    //compare if its greater swap a[i+1]=a[i]
+    //i++
 
-    sorrt(){
+    sorrt(arr){
+        let key, j
+        for(let i=1; i<arr.length; i++){
+            key=arr[i]
+            j=i-1
+            console.log("key,j", key,arr[j], "j", j, arr)
+            while(arr[j]>key){
+                arr[j+1]=arr[j]
+                j--
+            console.log("j",arr[j+1],arr)
+            }
+            arr[j+1]=key
+        }
+        console.log("arr", arr)
     }
 
 }
 
 
 var so=new insertsort()
-so.insertsort([5,3,4,1])
+so.sorrt([5,3,4,1])
